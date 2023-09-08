c5c61de619bfa3e5ff81ed965ccd0ae717c8fd10
Mohammedzainelabdein09 <zmhamd39@gmail.com>
2023-09-07 17:08:00 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main -1t prnits out output using put
+ *
+ * Return: Always (0)
+ *
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+
+}
