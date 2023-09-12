8aa5ac492e9f74c37961884083e2901229ef41d7
Mohammedzainelabdein09 <zmhamd39@gmail.com>
2023-09-12 13:34:23 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description:C programm use printf function
+ *
+ * Return:Always 0 (Success)
+ *
+*/
+
+int main(void)
+{
+	printf("_putchar");
+	return (0);
+}
