1c3c70909dd33fa7dd7c24f227af6c41e29ca6ab
Mohammedzainelabdein09 <zmhamd39@gmail.com>
2023-09-18 15:39:46 -0700

    task2

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+include"main.h"
+
+/**
+ * swp_int values of two intogers
+ * using two paramters
+ *
+ * @a: input paramter 1
+ * @b: input paramter 2
+ *
+ * Return: Nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+	int temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
