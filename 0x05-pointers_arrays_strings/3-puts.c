5ba044c75c61b16923cc590dd9d114d23d38554d
Mohammedzainelabdein09 <zmhamd39@gmail.com>
2023-09-18 23:35:56 -0700

    task4

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,7 +3,7 @@
 /**
  * _puts - prints a string, followod by a newline to stdout
  *
- * @str: string parameter to print 
+ * @str:  string parameter to print 
  *
  * Return: Nothing
 */
@@ -14,6 +14,7 @@ void _puts(char *str)
 	{
 	       _putchar(*str);
 	       str++;
-	       }
+
+}
 	_putchar('\n');
 }
