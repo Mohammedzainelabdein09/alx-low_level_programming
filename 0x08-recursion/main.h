466dfb0f94f0e849c43c9153af16ab386c26d10d
Mohammedzainelabdein09 <zmhamd39@gmail.com>
2023-09-28 16:03:46 -0700

    0. a function that prints a string, followed by a new line

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,8 @@
+int _putcgar(char c);
+void _putchar_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int wildcmp(char *s1, char *s2);
